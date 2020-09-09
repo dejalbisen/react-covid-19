066cc42e009b4c1f223697faf62adf52e823027e
harshwardhan <iharshwardhanchaudhary@gmail.com>
2020-09-09 22:55:39 +0530

    Updates

---
+++ precache-manifest.770a66c73fa2fa4a954843c4afe841ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ad5a2f0673d348b4ec91df54777fb4",
+    "url": "/react-covid-19/index.html"
+  },
+  {
+    "revision": "186dc1c4d60d18f69ace",
+    "url": "/react-covid-19/static/css/2.dc17f35a.chunk.css"
+  },
+  {
+    "revision": "3fffd9bde03bdf92237d",
+    "url": "/react-covid-19/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "186dc1c4d60d18f69ace",
+    "url": "/react-covid-19/static/js/2.011ff507.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/react-covid-19/static/js/2.011ff507.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fffd9bde03bdf92237d",
+    "url": "/react-covid-19/static/js/main.b2d9931e.chunk.js"
+  },
+  {
+    "revision": "c75dca038f37e67aafdb",
+    "url": "/react-covid-19/static/js/runtime-main.65867c61.js"
+  }
+]);
